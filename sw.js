cfd05c426d58e02c2659d1c0cb0ee12d5fc2bba7
subramanillrs <57629442+subramanillrs@users.noreply.github.com>
2026-08-28 22:54:11 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="g22-final-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./history.json","./manifest.webmanifest","./icon.svg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match("./index.html")))));
